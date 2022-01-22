60e9f61edb73e38a9845c0856e7944aef9a08b68
Aadesh <watadeadesh2062001@gmail>
2022-01-22 12:47:29 +0530

    minor fixes

--- src/components/admin/AddPost.js
+++ src/components/admin/AddPost.js
@@ -6,128 +6,128 @@ const AddPost = () => {
     <div className="flex flex-col">
       <div className="md:grid md:auto-rows-auto m-8 sm:flex sm:flex-row md:grid-cols-2 place-items-center w-3/4 mx-auto bg-white px-10 shadow-xl rounded-lg py-6">
         <h1 className="text-headings font-bold sm:text-xl md:text-2xl place-self-start mb-8 col-span-2 border-l-4 pl-4 border-headings">Add new post</h1>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="postType"
             >
               Post Type
             </label>
             <input
-              class="block  bg-white text-gray-700 border border-gray-500 w-11/12 rounded py-2 px-4 my-2"
+              className="block  bg-white text-gray-700 border border-gray-500 w-11/12 rounded py-2 px-4 my-2"
               id="postType"
               type="text"
               placeholder="Post Type"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="companyName"
             >
               Company name
             </label>
             <input
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="companyName"
               type="text"
               placeholder="Company Name"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="eligibility"
             >
               Eligibility
             </label>
             <input
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="eligibility"
               type="text"
               placeholder="Eligibility"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="position"
             >
               Position
             </label>
             <input
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="position"
               type="text"
               placeholder="Position"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="joining"
             >
               Joining
             </label>
             <input
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="joining"
               type="text"
               placeholder="Joining"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="deadline"
             >
               Deadline
             </label>
             <input
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="deadline"
               type="date"
               placeholder="Deadline"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full justify-center">
+        <div className="flex mx-auto w-full justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class=" text-gray-700 text-md font-bold place-self-start"
+              className=" text-gray-700 text-md font-bold place-self-start"
               htmlFor="date"
             >
               Date
             </label>
             <input
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="date"
               type="date"
               placeholder="Date"
             />
           </div>
         </div>
-        <div class="flex mx-auto w-full col-span-2 justify-center">
+        <div className="flex mx-auto w-full col-span-2 justify-center">
           <div className=" flex flex-col w-full my-4">
             <label
-              class="text-gray-700 text-md font-bold place-self-start"
+              className="text-gray-700 text-md font-bold place-self-start"
               htmlFor="content"
             >
               Content
             </label>
             <textarea
-              class="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
+              className="block bg-white text-gray-700 border border-gray-500  w-11/12 rounded py-2 px-4 my-2"
               id="content"
               type="text"
               placeholder="Content"
