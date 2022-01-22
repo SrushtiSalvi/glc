59a6b15aa6d0cce3cd6373ebc9dcacc8ad4389f2
Aadesh <watadeadesh2062001@gmail>
2022-01-22 19:07:00 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import ContactPage from "./pages/ContactPage";
 import Chairman from "./pages/Chairman";
 import Login from "./pages/Login";
 import RulesRegulations from "./pages/RulesRegulations";
+import NoticeBoard from "./pages/NoticeBoard";
+import AdminLayout from "./components/common/AdminLayout";
+import AddPost from "./components/admin/AddPost";
+import AllPosts from "./components/admin/AllPosts";
+import Notices from "./components/admin/Notices";
+import Blogs from "./components/admin/Blogs";
 
 function App() {
   return (
@@ -23,8 +29,14 @@ function App() {
           <Route path="contact" element={<ContactPage />} />
           <Route path="login" element={<Login />} />
           <Route path="rulesRegulations" element={<RulesRegulations />} />
+          <Route path="noticeBoard" element={<NoticeBoard />} />
         </Route>
-        {/* <Route path="/admin" element={<div>admin page</div>} /> */}
+        <Route path="admin" element={<AdminLayout />}>
+          <Route path="allPosts" element={<AllPosts />} />
+          <Route path="addPost" element={<AddPost />} />
+          <Route path="notices" element={<Notices />} />
+          <Route path="blogs" element={<Blogs />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
