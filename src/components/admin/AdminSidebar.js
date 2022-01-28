15e77b0d19e07e9bfde9cb3ac78907ab7779ca44
Srushti Salvi <srushtisalvi4@gmail.com>
2022-01-28 16:31:00 +0530

    [UPDATE] change theme page added

--- src/components/admin/AdminSidebar.js
+++ src/components/admin/AdminSidebar.js
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 import logo from '../../assets/logo2.png';
-import { IoIosPhotos } from 'react-icons/io';
+import { IoIosPhotos, IoMdSettings } from 'react-icons/io';
 import { BiNotepad } from 'react-icons/bi';
 import { ImBlog } from 'react-icons/im';
 import { BsPeopleFill } from 'react-icons/bs';
@@ -18,42 +18,52 @@ const AdminSidebar = () => {
         <NavLink
           to="/admin/allPosts"
           className={({ isActive }) => {
-            return ` flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-8 ${
+            return ` flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-4 ${
               isActive ? ' border-l-4' : ''
             }`;
           }}>
-          <IoIosPhotos className="mr-8 mt-1" />
+          <IoIosPhotos className="mr-6 mt-1" />
           POSTS
         </NavLink>
         <NavLink
           to="/admin/notices"
           className={({ isActive }) => {
-            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-8 ${
+            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-4 ${
               isActive ? ' border-l-4' : ''
             }`;
           }}>
-          <BiNotepad className="mr-8 mt-1" />
+          <BiNotepad className="mr-6 mt-1" />
           NOTICES
         </NavLink>
         <NavLink
           to="/admin/blogs"
           className={({ isActive }) => {
-            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-8 ${
+            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-4 ${
               isActive ? ' border-l-4' : ''
             }`;
           }}>
-          <ImBlog className="mr-8 mt-1" />
+          <ImBlog className="mr-6 mt-1" />
           BLOGS
         </NavLink>
         <NavLink
           to="/admin/subscribers"
           className={({ isActive }) => {
-            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-8 ${
+            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-4 ${
               isActive ? ' border-l-4' : ''
             }`;
           }}>
-          <BsPeopleFill className="mr-8 mt-1" />
+          <BsPeopleFill className="mr-6 mt-1" />
           SUBSCRIBERS
+        </NavLink>
+        <NavLink
+          to="/admin/themes"
+          className={({ isActive }) => {
+            return `flex text-gray-200 hover:bg-primary-dark transition-all duration-100 ease-out hover:text-white hover:border-l-4 py-4 sm:pl-3 lg:px-4 ${
+              isActive ? ' border-l-4' : ''
+            }`;
+          }}>
+          <IoMdSettings className="my-1 mr-6" />
+          CONFIGURATIONS
         </NavLink>
       </span>
     </div>
